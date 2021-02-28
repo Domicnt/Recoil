58111cadfef9df844b26f5a24bd80800d63cd504
Domic <dominicbertocchini@gmail.com>
2021-02-27 19:24:59 -0800

    added charge bar and projectile

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,5 +1,7 @@
 extends RigidBody2D
 
+const Projectile = preload("res://Scenes/Instances/Projectile.tscn");
+
 onready var cannon = get_node("Cannon");
 onready var particles = get_node("Cannon/Particles2D");
 onready var sprite = get_node("Sprite");
@@ -8,24 +10,31 @@ var time = OS.get_ticks_msec();
 var charging = false;
 
 func _ready():
-	pass
+	$Sprite2/Sprite3.region_rect.size.x = 0;
 
 func _physics_process(delta):
 	
 	var angle = get_viewport().get_mouse_position().angle_to_point(sprite.global_transform.origin + get_viewport().get_visible_rect().size / 2 - get_node("Camera2D").get_camera_position());
 	cannon.rotation = angle - rotation;
+	$Sprite2.rotation = -rotation;
 	
 	if Input.is_action_pressed("Fire") && !charging && OS.get_ticks_msec() - time > 200:
 		time = OS.get_ticks_msec();
 		charging = true;
 	
 	var charge_speed = 2;
-	if !Input.is_action_pressed("Fire") && charging:
-		var charge_time = OS.get_ticks_msec() - time;
-		charge_time = min(charge_time * charge_speed, 1000);
-		
-		particles.emitting = true;
-		apply_central_impulse(-charge_time * Vector2(cos(angle), sin(angle)));
-		time = OS.get_ticks_msec();
-		charging = false;
-		
+	if charging:
+		var charge_time = min((OS.get_ticks_msec() - time) * charge_speed, 1000);
+		$Sprite2/Sprite3.region_rect.size.x = (65 * charge_time / 1000) - 1;
+		if !Input.is_action_pressed("Fire"):
+			$Sprite2/Sprite3.region_rect.size.x = 0;
+			particles.emitting = true;
+			apply_central_impulse(-charge_time * Vector2(cos(angle), sin(angle)));
+			
+			var projectile = Projectile.instance();
+			Global.add_child(projectile);
+			projectile.global_position = self.global_position;
+			projectile.vel = Vector2(cos(angle), sin(angle)) * charge_time / 50;
+			
+			time = OS.get_ticks_msec();
+			charging = false;
--- Scripts/Projectile.gd
+++ Scripts/Projectile.gd
@@ -1,2 +1,6 @@
 extends Node2D
 
+var vel = Vector2.ZERO;
+
+func _physics_process(delta):
+	position += vel;
